c77058757c3bc6be2517a9cf6e3a1cc4e331bf8e
dandaradantas <dandaranavarro@gmail.com>
2018-04-03 23:14:04 -0300

    Uncomment service worker code and adding some files to cache

--- sw.pwa.js
+++ sw.pwa.js
@@ -5,17 +5,21 @@
     var CACHE_DATA = 'pwa-news-data-v1';
     var API = 'https://newsapi.org/v2/';
     var FILES_SHELL = [
-        './index.html',
+        '/',
         './assets/css/main.css',
         './libraries/bootstrap/bootstrap.min.css',
         './libraries/font-awesome/css/font-awesome.min.css',
+        './libraries/font-awesome/fonts/fontawesome-webfont.eot',
+        './libraries/font-awesome/fonts/fontawesome-webfont.ttf',
+        './libraries/font-awesome/fonts/fontawesome-webfont.woff',
         './app/api/api.js',
         './app/router/router.js',
         './app/modules/news/news.js',
         './libraries/jquery/jquery-3.3.1.min.js',
         './libraries/tether/tether.min.js',
         './libraries/bootstrap/bootstrap.min.js',
-        './libraries/moment/moment.min.js'
+        './libraries/moment/moment.min.js',
+        './assets/img/noimage.png'
     ]; 
 
     self.addEventListener('install', function(event){
