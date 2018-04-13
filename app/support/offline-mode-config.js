8bcc69a4db1d731b59f7c0ddf1b0a7cc53890af0
Dandara Navarro <dandara.navarro@virtus.ufcg.edu.br>
2018-04-13 17:28:04 -0300

    Adding online state feature - fixing

--- app/support/offline-mode-config.js
+++ app/support/offline-mode-config.js
@@ -5,14 +5,8 @@
     var onlineMessage = 'Your internet came back. You can reload the page and see the latest news.'
     var message = offlineMessage;
 
-    var modalTemplate = '<div class="modal-dialog"><div class="modal-content"><div class="modal-header">' +
-                        '<button type="button" class="close" data-dismiss="modal">&times;</button></div>' + 
-                        '<div class="modal-body"><p id="modal-message"></p></div>' +
-                        '<div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>';
-
     window.addEventListener('offline', function(){
         message = offlineMessage;
-        document.getElementById('internet-modal').innerHTML = modalTemplate;
         document.getElementById('modal-message').innerHTML = message;
         $('#internet-modal').modal();
     });
